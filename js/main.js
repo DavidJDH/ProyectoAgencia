1e54534f2eed297924b56240c130be326775e4a8
DavidJDH <ericjaramillod26@gmail.com>
2025-03-22 20:58:02 -0500

    parallax, leyout, nuevo fondo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Cambio de header
     $(window).on("scroll", function(){
         let posicion = $("#pr").offset()
         if ($(window).scrollTop() >= posicion.top) {
@@ -8,8 +9,19 @@ $(document).ready(function(){
         }
     })
 
+    //Accionamiento del Menu lateral
     $("#burger").click(function(){
         $("#burger").toggleClass("abierto")
         $(".menu").toggleClass("abierto")
+    })
+
+    //Smooth Scroll
+    $("a").on("click", function(){
+        if(this.hash !== "") {
+            let hash = this.hash;
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top
+            }, 800)
+        }
     })
 })
